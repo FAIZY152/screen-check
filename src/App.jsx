fa855b935c28ef55efe306126c3cdacb2f7a9afc
Muhammad Fayaz <141697766+FAIZY152@users.noreply.github.com>
2026-02-21 13:10:31 +0500

    Clarify comment about screen blur
    Updated comment to clarify screen blur functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import LandingPage from "./components/Main/component/LandingPage";
 import usePersistScreenshotBlackout from "./components/Main/component/PreventScreenshot";
-// screen blur show
+// screen blur 
 const App = () => {
   usePersistScreenshotBlackout();
   return (
